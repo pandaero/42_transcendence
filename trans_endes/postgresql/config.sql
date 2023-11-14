c08f5acef5a20252169942d798fde6bf98613a6b
dodo <dragosimbrea@gmail.com>
2023-11-14 19:55:26 +0200

    update

--- trans_endes/postgresql/config.sql
+++ trans_endes/postgresql/config.sql
@@ -1,15 +1,15 @@
-CREATE USER admin_user WITH PASSWORD 'docker';
+-- CREATE USER admin_user WITH PASSWORD 'docker';
 
-CREATE DATABASE transcendence_db WITH OWNER admin_user;
+-- CREATE DATABASE transcendence_db WITH OWNER admin_user;
 
 CREATE TABLE users(
 	userKeyId SERIAL PRIMARY KEY,
-	email VARCHAR(30),
-	pwd VARCHAR(30),
-	userName VARCHAR(30),
-	firstName VARCHAR(30),
-	lastName VARCHAR(30),
-	avatar VARCHAR(255),
+	email VARCHAR,
+	pwd VARCHAR,
+	userName VARCHAR,
+	firstName VARCHAR,
+	lastName VARCHAR,
+	avatar VARCHAR,
 	games INT,
 	wins INT,
 	losses INT,
@@ -18,13 +18,13 @@ CREATE TABLE users(
 
 CREATE TABLE history(
 	gameID	SERIAL PRIMARY KEY,
-	playerOne	VARCHAR(30),
-	playerTwo	VARCHAR(30),
+	playerOne	VARCHAR,
+	playerTwo	VARCHAR,
 	enemyScore	INT,
 	ownScore	INT
 );
 
-GRANT ALL PRIVILEGES ON DATABASE transcendence_db TO admin_user;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin_user;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO admin_user;
-GRANT CREATE ON SCHEMA public TO admin_user;
+-- GRANT ALL PRIVILEGES ON DATABASE transcendence_db TO admin_user;
+-- GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin_user;
+-- GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO admin_user;
+-- GRANT CREATE ON SCHEMA public TO admin_user;
